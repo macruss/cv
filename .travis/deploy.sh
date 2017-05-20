b1a766718bd68b6f2c4a96bcfd9e7c8b10916a74
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2017-05-21 00:56:49 +0300

    test travis (7) - deploy dist

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,10 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
+
+    cd dist
+
+    git init
     git remote add deploy "travis@188.166.80.175:/var/www/cv.macruss.pp.ua"
     git config user.name "Travis CI"
     git config user.email "travis@macruss.pp.ua"
